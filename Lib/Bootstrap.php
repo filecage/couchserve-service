c838ce43a51d629fadde2e603b5a11c31b34e580
filecage <filecage@users.noreply.github.com>
2014-10-05 01:34:27 +0200

    bootstrap can now load modules

--- Lib/Bootstrap.php
+++ Lib/Bootstrap.php
@@ -1,15 +1,35 @@
 <?php
 
     namespace couchService\Service\Lib;
+    use couchService\Service\Lib\Abstracts\Module;
 
     class Bootstrap {
 
+        /**
+         * @var ModuleRegistry
+         */
+        protected $moduleRegistry;
+
         public function __construct() {
+            $this->moduleRegistry = new ModuleRegistry();
+        }
+
+        public function loadApp() {
             $this->loadConfiguration();
+            $this->loadModules();
         }
 
         protected function loadConfiguration() {
+            Configuration::prepare(Supplier::getReusableDatabase());
+        }
 
+        protected function loadModules() {
+            foreach (Configuration::getModules() as $configurationRow) {
+                /** @var Module $module */
+                $module = new $configurationRow['type'];
+                $module->injectConfigurationRow($configurationRow);
+                $this->moduleRegistry->registerModule($module, $configurationRow);
+            }
         }
 
     }
--- service.php
+++ service.php
@@ -1,7 +1,9 @@
 <?php
 
-    require_once 'Exceptions/AutoloadException.php';
+    require_once 'Lib/Exceptions/AutoloadException.php';
     require_once 'Lib/Autoload.php';
 
-    spl_autoload_register('\couchService\Service\Lib\Autoload::load');
+    spl_autoload_register('couchService\Service\Lib\Autoload::load');
 
+    $bootstrap = new couchService\Service\Lib\Bootstrap();
+    $bootstrap->loadApp();
