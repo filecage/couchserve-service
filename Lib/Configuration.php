899b2c9487b025dbd34806ded70e61093f277e4a
filecage <filecage@users.noreply.github.com>
2014-10-05 00:44:22 +0200

    adds load methods for modules and environment var

--- Lib/Configuration.php
+++ Lib/Configuration.php
@@ -4,8 +4,39 @@
 
     class Configuration {
 
-        public static function prepare() {
+        protected static $prepared = false;
+        protected static $environment = [];
+        protected static $modules = [];
 
+        /**
+         * @param Database $db
+         */
+        public static function prepare($db) {
+            if (static::$prepared) {
+                return;
+            }
+
+            static::loadEnvironment($db);
+            static::loadModules($db);
+            static::$prepared = true;
+        }
+
+        /**
+         * @param Database $db
+         */
+        protected static function loadEnvironment($db) {
+            foreach ($db->getArray('SELECT envKey, value FROM environment') as $row) {
+                static::$environment[$row['envKey']] = $row['value'];
+            }
+        }
+
+        /**
+         * @param Database $db
+         */
+        protected static function loadModules($db) {
+            foreach ($db->getArray('SELECT id, name, type FROM modules WHERE active = 1') as $row) {
+                static::$modules[$row['id']] = $row;
+            }
         }
 
     }
