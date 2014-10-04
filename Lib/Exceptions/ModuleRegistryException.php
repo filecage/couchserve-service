bcba30363583e9783f73362f4f1b6c718e6646e3
filecage <filecage@users.noreply.github.com>
2014-10-05 01:15:01 +0200

    adds ModuleRegistry to globally register modules

---
+++ Lib/Exceptions/ModuleRegistryException.php
@@ -0,0 +1,4 @@
+<?php
+
+    namespace couchService\Service\Lib\Exceptions;
+    class ModuleRegistryException extends \Exception {}
---
+++ Lib/ModuleRegistry.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace couchService\Service\Lib;
+    use couchService\Service\Lib\Abstracts\Module;
+    use couchService\Service\Lib\Exceptions\ModuleRegistryException;
+
+    class ModuleRegistry {
+
+        /**
+         * @var Module[]
+         */
+        protected $modules = [];
+
+        /**
+         * @param Module $module
+         * @param array $configurationRow
+         * @throws ModuleRegistryException
+         */
+        public function registerModule(Module $module, Array $configurationRow) {
+            if (isset($this->modules[$configurationRow['id']])) {
+                throw new ModuleRegistryException('Module already registered, multiple registrations are not allowed');
+            }
+            $this->modules[$configurationRow['id']] = $module;
+            $module->injectConfigurationRow($configurationRow);
+            $module->register();
+        }
+
+    }
