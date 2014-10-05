fb9a589ecf3fb78813472dac363daadd29c544cf
filecage <filecage@users.noreply.github.com>
2014-10-05 03:37:21 +0200

    sensors don't have to implement the act method

--- Lib/Abstracts/Sensor.php
+++ Lib/Abstracts/Sensor.php
@@ -1,7 +1,10 @@
 <?php
 
     namespace couchServe\Service\Lib\Abstracts;
+    use couchServe\Service\Lib\Command;
 
     abstract class Sensor extends Module {
         public abstract function sense();
+
+        public function act(Command $command) { }
     }
