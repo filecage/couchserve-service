96c1ee4f48c3171e04f6d44a6aeba9131a2b60f3
filecage <filecage@users.noreply.github.com>
2014-10-05 03:49:27 +0200

    add anonymous or app-intern targets

--- Lib/Command.php
+++ Lib/Command.php
@@ -25,56 +25,63 @@
         protected $data = [];
 
         /**
-         * @return mixed
+         * @return Array
          */
         public function getData() {
             return $this->data;
         }
 
         /**
-         * @param mixed $data
+         * @param Array $data
          */
         public function setData(Array $data) {
             $this->data = $data;
         }
 
         /**
-         * @return string
+         * @return String
          */
         public function getName() {
             return $this->name;
         }
 
         /**
-         * @param string $name
+         * @param String $name
          */
         public function setName($name) {
             $this->name = $name;
         }
 
         /**
-         * @return string
+         * @return String
          */
         public function getTargetId() {
             return $this->targetId;
         }
 
         /**
-         * @param string $targetId
+         * @param Int $targetId
          */
         public function setTargetId($targetId) {
+            $this->targetId = APP_ID . '_' . $targetId;
+        }
+
+        /**
+         * @param String $targetId
+         */
+        public function setTargetIdAnonymous($targetId) {
             $this->targetId = $targetId;
         }
 
         /**
-         * @return string
+         * @return String
          */
         public function getType() {
             return $this->type;
         }
 
         /**
-         * @param string $type
+         * @param String $type
          */
         public function setType($type) {
             $this->type = $type;
