148fb756391309b56dacbc9e5d18f2519e3a534c
filecage <filecage@users.noreply.github.com>
2014-10-14 00:21:01 +0200

    adds getter method for senses

--- Lib/Abstracts/Sensor.php
+++ Lib/Abstracts/Sensor.php
@@ -4,7 +4,15 @@
     use couchServe\Service\Lib\Command;
 
     abstract class Sensor extends Module {
+
+        /**
+         * @return $this
+         */
         public abstract function sense();
+
+        public function getSenses() {
+            return $this->senses;
+        }
 
         public function act(Command $command) { }
     }
