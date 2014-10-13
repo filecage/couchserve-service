dcbfe431fa3ed4636566220ef89bac8e1ee278aa
filecage <filecage@users.noreply.github.com>
2014-10-14 00:01:56 +0200

    adds basic functionality to sense class

--- Lib/Sense.php
+++ Lib/Sense.php
@@ -1,7 +1,43 @@
 <?php
 
     namespace couchServe\service\Lib;
+    use couchServe\service\Lib\Abstracts\Sensor;
 
     class Sense {
+
+        /**
+         * @var Sensor
+         */
+        protected $sensor;
+
+        /**
+         * @var mixed
+         */
+        protected $value;
+
+        /**
+         * @param $value
+         * @param Sensor $sensor
+         */
+        public function __construct($value, Sensor $sensor = null) {
+            $this->value = $value;
+            if ($sensor) {
+                $this->sensor = $sensor;
+            }
+        }
+
+        /**
+         * @return Sensor
+         */
+        public function getSensor() {
+            return $this->sensor;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getValue() {
+            return $this->value;
+        }
 
     }
