52119cdca0456dc0e6f7f678c80cb36f5f8daaf1
filecage <filecage@users.noreply.github.com>
2014-10-14 01:15:26 +0200

    dispatch command on each 50000. tick

--- Controller/Dummy.php
+++ Controller/Dummy.php
@@ -3,7 +3,7 @@
     namespace couchServe\service\Controller;
     use couchServe\service\Lib\Abstracts\Controller;
     use couchServe\service\Lib\CommandPool;
-    use couchServe\service\Lib\Sense;
+    use couchServe\service\Lib\Command;
     use couchServe\service\Lib\Log;
 
     class Dummy extends Controller {
@@ -16,12 +16,19 @@
 
         public function react(CommandPool $commandPool) {
             foreach ($commandPool->getSenses() as $sense) {
-                if($sense->getSensor() instanceof \couchServe\service\Sensors\Dummy) {
+                if ($sense->getSensor() instanceof \couchServe\service\Sensors\Dummy) {
                     if($sense->getValue() % 50000 === 0) {
                         $this->state++;
+                        $this->addNewDummyCommand($commandPool);
                     }
                 }
             }
+        }
+
+        protected function addNewDummyCommand(CommandPool $commandPool) {
+            $command = new Command;
+            $command->setTargetByModule($this->moduleRegistry->findModuleByTypeAndName(\couchServe\service\Modules\Dummy::class, 'dummy'));
+            $commandPool->addCommand($command);
         }
 
     }
