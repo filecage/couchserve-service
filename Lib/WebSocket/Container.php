67bfa7f20ee1731e82a644608392b3216e67de32
filecage <filecage@users.noreply.github.com>
2014-10-16 01:41:44 +0200

    added websocket container wrapper

---
+++ Lib/WebSocket/Container.php
@@ -0,0 +1,44 @@
+<?php
+
+    namespace couchServe\service\Lib\WebSocket;
+    use couchServe\service\Lib\WebSocket\Lib\Server;
+
+    class Container {
+
+        const HANDLER_APP_KEY = 'couchServe';
+
+        /**
+         * @var Server
+         */
+        protected $server;
+
+        /**
+         * @var Handler
+         */
+        protected $handler;
+
+        /**
+         * @param Server $server
+         * @param Handler $handler
+         */
+        public function __construct(Server $server, Handler $handler) {
+            $server->registerApplication(self::HANDLER_APP_KEY, $handler);
+            $this->server = $server;
+            $this->handler = $handler;
+        }
+
+        /**
+         * @return Handler
+         */
+        public function getHandler() {
+            return $this->handler;
+        }
+
+        /**
+         * @return Server
+         */
+        public function getServer() {
+            return $this->server;
+        }
+
+    }
