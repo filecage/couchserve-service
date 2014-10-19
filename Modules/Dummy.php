5cce7d2c8ea8065c3731857b2fd2ec7aa29d2170
filecage <filecage@users.noreply.github.com>
2014-10-20 01:01:49 +0200

    Dummy implements TogglePercent

--- Modules/Dummy.php
+++ Modules/Dummy.php
@@ -2,17 +2,28 @@
 
     namespace couchServe\service\Modules;
     use couchServe\service\Lib\Abstracts\Module;
+    use couchServe\service\Lib\ModuleValues\TogglePercent;
     use couchServe\service\Lib\Command;
     use couchServe\service\Lib\Log;
 
     class Dummy extends Module {
 
-        public function __construct(){
+        public function __construct() {
+            $this->moduleValue = new TogglePercent($this);
             Log::info('Dummy module loaded');
         }
 
         public function act(Command $command) {
-            Log::info('Dummy module is supposed to do anything...');
+            $data = $command->getData();
+            if (empty($data)) {
+                return;
+            }
+
+            $value = array_shift($data);
+            $this->moduleValue->setValue($value);
+            Log::info('Dummy modules value set to %d', [
+                $this->moduleValue->getCurrentValue()
+            ]);
         }
 
     }
