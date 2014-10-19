973c5404b49f55ba03279c7d4c27c105a151c677
filecage <filecage@users.noreply.github.com>
2014-10-20 00:20:14 +0200

    Sensor should not inherit from Module

--- Lib/Abstracts/Sensor.php
+++ Lib/Abstracts/Sensor.php
@@ -2,10 +2,29 @@
 
     namespace couchServe\Service\Lib\Abstracts;
     use couchServe\Service\Lib\Group;
-    use couchServe\Service\Lib\Command;
     use couchServe\Service\Lib\Sense;
 
-    abstract class Sensor extends Module {
+    abstract class Sensor {
+
+        /**
+         * @var Int
+         */
+        protected $id;
+
+        /**
+         * @var String
+         */
+        protected $type;
+
+        /**
+         * @var String
+         */
+        protected $name;
+
+        /**
+         * @var Array
+         */
+        protected $configurationRow;
 
         /**
          * @var Sense[]
@@ -27,10 +46,41 @@
         }
 
         /**
+         * @param array $row
+         */
+        public function injectConfigurationRow(Array $row) {
+            $this->configurationRow = $row;
+            $this->type = $row['type'];
+            $this->name = $row['name'];
+            $this->id = $row['id'];
+        }
+
+        /**
+         * @return String
+         */
+        public function getName() {
+            return $this->name;
+        }
+
+        /**
+         * @return String
+         */
+        public function getType() {
+            return $this->type;
+        }
+
+        /**
          * @return Group
          */
         public function getGroup() {
             return $this->group;
+        }
+
+        /**
+         * @return Int
+         */
+        public function getId() {
+            return $this->id;
         }
 
         /**
@@ -41,5 +91,8 @@
             $this->group->registerSensor($this);
         }
 
-        public function act(Command $command) { }
+        public function register() { }
+
+        public function unregister() { }
+
     }
